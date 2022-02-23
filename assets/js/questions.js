b018c54ecc28e37ca76b28dbe96b77371d068956
AlexNexton <64314387+AlexNexton@users.noreply.github.com>
2022-02-23 00:12:28 +0000

    doc: changed the answer to turn green

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -20,237 +20,237 @@ let questions = [
       "It's sleeping."
       
     ]
-  // },
-  //   {
-  //   numb: 3,
-  //   question: "I hope it doesn't rain.",
-  //   answer: "So do I.",
-  //   options: [
-  //     "Of course not.",
-  //     "Will it be wet?",
-  //     "So do I."
-  //   ]
-  // },
-  //   {
-  //   numb: 4,
-  //   question: "Are you going to come inside soon?",
-  //   answer: "In a minute.",
-  //   options: [
-  //     "For ever.",
-  //     "Not long.",
-  //     "In a minute."
-  //   ]
-  // },
-  //   {
-  //   numb: 5,
-  //   question: "Who gave you this book, Lucy?",
-  //   answer: "I bought it.",
-  //   options: [
-  //     "I bought it.",
-  //     "For my birthday.",
-  //     "My uncle was."
-  //   ]
-  // },
-  //     {
-  //   numb: 6,
-  //   question: "Shall we go out for pizza tonight?",
-  //   answer: "I'm too tired.",
-  //   options: [
-  //     "I know that.",
-  //     "It's very good.",
-  //     "I'm too tired."
-  //   ]
-  // },
-  //     {
-  //   numb: 7,
-  //   question: "Do you mind if I come too?",
-  //   answer: "That's fine!",
-  //   options: [
-  //     "I'd like to.",
-  //     "That's fine!",
-  //     "I don't know if I can."
-  //   ]
-  // },
-  //     {
-  //   numb: 8,
-  //   question: "There's someone at the door.",
-  //   answer: "Well, go and answer it then.",
-  //   options: [
-  //     "Can I help you?",
-  //     "Well, go and answer it then.",
-  //     "He's busy at the moment."
-  //   ]
-  // },
-  //     {
-  //   numb: 9,
-  //   question: "How much butter do I need for this cake?",
-  //   answer: "I'm not sure.",
-  //   options: [
-  //     "I'd like one.",
-  //     "I'll use some.",
-  //     "I'm not sure."
-  //   ]
-  // },
-  //     {
-  //   numb: 10,
-  //   question: "How long are you here for?",
-  //   answer: "Till tomorrow.",
-  //   options: [
-  //     "Since last week.",
-  //     "Ten days ago.",
-  //     "Till tomorrow."
-  //   ]
-  // },
-  // {
-  //   numb: 11,
-  //   question: "Have you guys had enough to eat?",
-  //   answer: "Is there any more rice?",
-  //   options: [
-  //     "That's all right.",
-  //     "Is there any more rice?",
-  //     "It's not the right time."
-  //   ]
-  // },
-  // {
-  //   numb: 12,
-  //   question: "That's my coat over there.",
-  //   answer: "Here you are.",
-  //   options: [
-  //     "Will you take it off?",
-  //     "No, you haven't!",
-  //     "Here you are."
-  //   ]
-  // },
-  // {
-  //   numb: 13,
-  //   question: "Let's go by bus.",
-  //   answer: "It'll take too long.",
-  //   options: [
-  //     "The train was expensive.",
-  //     "We'll buy a ticket.",
-  //     "It'll take too long."
-  //   ]
-  // },
-  // {
-  //   numb: 14,
-  //   question: "Do you know my brother Charlie?",
-  //   answer: "I don't think I do.",
-  //   options: [
-  //     "Sorry, he's not here.",
-  //     "I don't think I do.",
-  //     "I know."
-  //   ]
-  // },
-  // {
-  //   numb: 15,
-  //   question: "Would you like some ice in your drink or not?",
-  //   answer: "I don't mind.",
-  //   options: [
-  //     "I hope so.",
-  //     "Yes, I shall.",
-  //     "I don't mind."
-  //   ]
-  // },
-  // {
-  //   numb: 16,
-  //   question: "I hope I haven't ...... you any trouble by changing the arrangements.",
-  //   answer: "caused",
-  //   options: [
-  //     "put",
-  //     "caused",
-  //     "made",
-  //     "done"
-  //   ]
-  // },
-  // {
-  //   numb: 17,
-  //   question: "The floor is wet: don't run or you might ...... !",
-  //   answer: "slip",
-  //   options: [
-  //     "stoop",
-  //     "spill",
-  //     "slip",
-  //     "spin"
-  //   ]
-  // },  {
-  //   numb: 18,
-  //   question: "When you come to my house, ...... your camera with you.",
-  //   answer: "bring",
-  //   options: [
-  //     "take",
-  //     "show",
-  //     "fetch",
-  //     "bring"
-  //   ]
-  // },  {
-  //   numb: 19,
-  //   question: "Paul arrived at the shop ....... as the manager was closing for the day.",
-  //   answer: "just",
-  //   options: [
-  //     "even",
-  //     "just",
-  //     "still",
-  //     "right"
-  //   ]
-  // },  {
-  //   numb: 20,
-  //   question: "I would ...... to stay at home and relax for a change.",
-  //   answer: "prefer",
-  //   options: [
-  //     "rather",
-  //     "better",
-  //     "prefer",
-  //     "enjoy"
-  //   ]
-  // }, {
-  //   numb: 21,
-  //   question: "Is there ...... of food for everyone?",
-  //   answer: "plenty",
-  //   options: [
-  //     "adequate",
-  //     "enough",
-  //     "sufficient",
-  //     "plenty"
-  //   ]
-  // }, {
-  //   numb: 22,
-  //   question: "Lily says she's happy at school but she's ...... complaining.",
-  //   answer: "always",
-  //   options: [
-  //     "rarely",
-  //     "sometimes",
-  //     "always",
-  //     "often"
-  //   ]
-  // }, {
-  //   numb: 23,
-  //   question: "...... the step when you go in.",
-  //   answer: "Mind",
-  //   options: [
-  //     "Consider",
-  //     "Mind",
-  //     "Attend",
-  //     "Look"
-  //   ]
-  // }, {
-  //   numb: 24,
-  //   question: "...... stay the night if it's too difficult to get home.",
-  //   answer: "By all means",
-  //   options: [
-  //     "At all costs",
-  //     "By all means",
-  //     "In all",
-  //     "On the whole"
-  //   ]
-  // }, {
-  //   numb: 25,
-  //   question: "No ...... Hannah is happy when you think how many prizes she has won recently.",
-  //   answer: "wonder",
-  //   options: [
-  //     "surprise",
-  //     "problem",
-  //     "question",
-  //     "wonder"
-  //   ]
+  },
+    {
+    numb: 3,
+    question: "I hope it doesn't rain.",
+    answer: "So do I.",
+    options: [
+      "Of course not.",
+      "Will it be wet?",
+      "So do I."
+    ]
+  },
+    {
+    numb: 4,
+    question: "Are you going to come inside soon?",
+    answer: "In a minute.",
+    options: [
+      "For ever.",
+      "Not long.",
+      "In a minute."
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who gave you this book, Lucy?",
+    answer: "I bought it.",
+    options: [
+      "I bought it.",
+      "For my birthday.",
+      "My uncle was."
+    ]
+  },
+      {
+    numb: 6,
+    question: "Shall we go out for pizza tonight?",
+    answer: "I'm too tired.",
+    options: [
+      "I know that.",
+      "It's very good.",
+      "I'm too tired."
+    ]
+  },
+      {
+    numb: 7,
+    question: "Do you mind if I come too?",
+    answer: "That's fine!",
+    options: [
+      "I'd like to.",
+      "That's fine!",
+      "I don't know if I can."
+    ]
+  },
+      {
+    numb: 8,
+    question: "There's someone at the door.",
+    answer: "Well, go and answer it then.",
+    options: [
+      "Can I help you?",
+      "Well, go and answer it then.",
+      "He's busy at the moment."
+    ]
+  },
+      {
+    numb: 9,
+    question: "How much butter do I need for this cake?",
+    answer: "I'm not sure.",
+    options: [
+      "I'd like one.",
+      "I'll use some.",
+      "I'm not sure."
+    ]
+  },
+      {
+    numb: 10,
+    question: "How long are you here for?",
+    answer: "Till tomorrow.",
+    options: [
+      "Since last week.",
+      "Ten days ago.",
+      "Till tomorrow."
+    ]
+  },
+  {
+    numb: 11,
+    question: "Have you guys had enough to eat?",
+    answer: "Is there any more rice?",
+    options: [
+      "That's all right.",
+      "Is there any more rice?",
+      "It's not the right time."
+    ]
+  },
+  {
+    numb: 12,
+    question: "That's my coat over there.",
+    answer: "Here you are.",
+    options: [
+      "Will you take it off?",
+      "No, you haven't!",
+      "Here you are."
+    ]
+  },
+  {
+    numb: 13,
+    question: "Let's go by bus.",
+    answer: "It'll take too long.",
+    options: [
+      "The train was expensive.",
+      "We'll buy a ticket.",
+      "It'll take too long."
+    ]
+  },
+  {
+    numb: 14,
+    question: "Do you know my brother Charlie?",
+    answer: "I don't think I do.",
+    options: [
+      "Sorry, he's not here.",
+      "I don't think I do.",
+      "I know."
+    ]
+  },
+  {
+    numb: 15,
+    question: "Would you like some ice in your drink or not?",
+    answer: "I don't mind.",
+    options: [
+      "I hope so.",
+      "Yes, I shall.",
+      "I don't mind."
+    ]
+  },
+  {
+    numb: 16,
+    question: "I hope I haven't ...... you any trouble by changing the arrangements.",
+    answer: "caused",
+    options: [
+      "put",
+      "caused",
+      "made",
+      "done"
+    ]
+  },
+  {
+    numb: 17,
+    question: "The floor is wet: don't run or you might ...... !",
+    answer: "slip",
+    options: [
+      "stoop",
+      "spill",
+      "slip",
+      "spin"
+    ]
+  },  {
+    numb: 18,
+    question: "When you come to my house, ...... your camera with you.",
+    answer: "bring",
+    options: [
+      "take",
+      "show",
+      "fetch",
+      "bring"
+    ]
+  },  {
+    numb: 19,
+    question: "Paul arrived at the shop ....... as the manager was closing for the day.",
+    answer: "just",
+    options: [
+      "even",
+      "just",
+      "still",
+      "right"
+    ]
+  },  {
+    numb: 20,
+    question: "I would ...... to stay at home and relax for a change.",
+    answer: "prefer",
+    options: [
+      "rather",
+      "better",
+      "prefer",
+      "enjoy"
+    ]
+  }, {
+    numb: 21,
+    question: "Is there ...... of food for everyone?",
+    answer: "plenty",
+    options: [
+      "adequate",
+      "enough",
+      "sufficient",
+      "plenty"
+    ]
+  }, {
+    numb: 22,
+    question: "Lily says she's happy at school but she's ...... complaining.",
+    answer: "always",
+    options: [
+      "rarely",
+      "sometimes",
+      "always",
+      "often"
+    ]
+  }, {
+    numb: 23,
+    question: "...... the step when you go in.",
+    answer: "Mind",
+    options: [
+      "Consider",
+      "Mind",
+      "Attend",
+      "Look"
+    ]
+  }, {
+    numb: 24,
+    question: "...... stay the night if it's too difficult to get home.",
+    answer: "By all means",
+    options: [
+      "At all costs",
+      "By all means",
+      "In all",
+      "On the whole"
+    ]
+  }, {
+    numb: 25,
+    question: "No ...... Hannah is happy when you think how many prizes she has won recently.",
+    answer: "wonder",
+    options: [
+      "surprise",
+      "problem",
+      "question",
+      "wonder"
+    ]
    },
 ];
